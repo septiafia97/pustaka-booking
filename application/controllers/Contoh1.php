aa829157afdb7df5ae6f608998a4ecf07ac2f97e
septiafia97 <septiafia98@gmail.com>
2023-10-12 23:48:22 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Alifia Septiani Salsabila
+            Saya tinggal di daerah Magelang
+            Olahraga yang saya sukai adalah bulutangkis";
+    }
+}
