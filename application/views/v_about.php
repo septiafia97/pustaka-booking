38d96cb7d8c4463ca3c34ce45091bb9eda224a53
septiafia97 <145754125+septiafia97@users.noreply.github.com>
2023-10-20 09:38:55 +0700

    Add files via upload

---
+++ application/views/v_about.php
@@ -0,0 +1,27 @@
+<section>
+    
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Alifia</li>
+        <li>Nama Belakang : Salsabila</li>
+    </ul>
+
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Merbabu II Nusukan Banjarsari</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Magelang</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Bola Kasti</li>
+    </ul>
+
+</section>
